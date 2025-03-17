5b2ea1e7d1565ff06d673d209e8b21038b0e6534
023bqmx <bombeamlazer@gmail.com>
2025-03-17 16:22:16 +0700

    Am i negative

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+number = int(input("Input your number : "))
+
+if number < 0 :
+    print("This number is negative")
+elif number > 0 :
+    print("This number is positive")
+else:
+    print("The number is both positive and negative")
