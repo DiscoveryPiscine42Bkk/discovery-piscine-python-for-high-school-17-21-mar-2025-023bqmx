e3196ead40448bb71c047406ee0d7b7d1f520c91
023bqmx <bombeamlazer@gmail.com>
2025-03-18 09:52:00 +0700

    age4.1

---
+++ cell04/ex01/age.py
@@ -0,0 +1,9 @@
+age = int(input("Please tell me your age : "))
+
+a = age + 10
+b = age + 20
+c = age + 30
+
+print("In 10 years, you'll be",a, "years")
+print("In 20 years, you'll be",b, "years")
+print("In 30 years, you'll be",c, "years")
