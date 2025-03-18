69be179be99a87e60761b2468a0021e033556467
023bqmx <bombeamlazer@gmail.com>
2025-03-18 16:17:58 +0700

    uplow4.5

--- cell04/ex05/up_low.py
+++ cell04/ex05/up_low.py
@@ -1,17 +1,6 @@
-def swap_case(s):
+#!/usr/bin/env python3
 
-    result = ""
-    for char in s:
-        if char.isupper():
-            result += char.lower()
-        elif char.islower():
-            result += char.upper()
-        else:
-            result += char 
+Text = str(input(" "))
+swap = Text.swapcase()
 
-    return result
-
-if __name__ == "__main__":
-    input_string = input("In put your word : ")
-    output_string = swap_case(input_string)
-    print("Your word has been change to : " ,output_string)
+print(swap)
--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,6 +1,7 @@
 array01 =  [2, 8, 9, 48, 8, 22, -12, 2]
 array02 = [i+2  for i in array01]
-sum_array = [i for i in array02 if i>=10]
+# sum_array = [i for i in array02 if i>=10]
+sum_array = list(filter(lambda x: x >= 10, array02))
 
 print(array01)
 print(sum_array)
