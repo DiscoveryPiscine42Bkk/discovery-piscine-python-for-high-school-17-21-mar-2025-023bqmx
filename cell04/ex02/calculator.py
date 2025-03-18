f6628d8b56ac65d1dfc9541bdebf185a88e87d80
023bqmx <bombeamlazer@gmail.com>
2025-03-18 11:04:30 +0700

    calculator

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,8 +1,15 @@
-num1 = int(input("Give me the first number : "))
-num2 = int(input("Give me your second number : "))
-print("Thank you!")
+print("Give me the first number: ")
+num1 = int(input())
+print("Give me the second number: ")
+num2 = int(input())
+print("Thank You!")
+try:
+    print(f"{num1} + {num2} = {num1 + num2}")
+    print(f"{num1} - {num2} = {num1 - num2}")
+    print(f"{num1} / {num2} = {num1 / num2}") 
+    print(f"{num1} * {num2} = {num1 * num2}") 
 
-print(f"{num1} + {num2} = {num1 + num2}")
-print(f"{num1} - {num2} = {num1 - num2}")
-print(f"{num1} / {num2} = {num1 / num2}")
-print(f"{num1} * {num2} = {num1 * num2}")
+except ZeroDivisionError:
+    print(f"{num1} / {num2} = Undefined")
+
+    print(f"{num1} x {num2} = {num1 * num2}")
