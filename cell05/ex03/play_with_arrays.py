9dd74d8c7f81d4b863135633d2c0e538cac0d6f1
023bqmx <bombeamlazer@gmail.com>
2025-03-19 11:30:24 +0700

    cell05

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-def play_with_arrays(arr):
+origin = [2, 8, 9, 48, 8, 22, -12, 2]
+new = [i+2 for i in origin]
 
-    unique_elements = set(arr)
-    
-    unique_list = list(unique_elements)
-    
-    return unique_list
+sum = {i for i in new if i >5}
 
-original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-result = play_with_arrays(original_array)
-print(result)
+print(origin)
+print(sum)
---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) == 2:
+    para = sys.argv[1]
+    userinput = input("What was the parameter? ")
+
+    if para == userinput
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+
+elif len(sys.argv) != 2:
+    print("none")
